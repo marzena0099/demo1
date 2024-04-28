3fa4ccf80fe8be80584df9e1c0eef034618274d2
marzenusia <nsnjjs@com.pl>
2024-04-28 17:00:51 +0200

    kafka

--- encoder/src/main/java/com/example/demo/prohibited/ProhibitedService.java
+++ encoder/src/main/java/com/example/demo/prohibited/ProhibitedService.java
@@ -3,12 +3,18 @@ package com.example.demo.prohibited;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 @AllArgsConstructor
 public class ProhibitedService {
     private final ProhibitedRepository prohibitedRepository;
 
-    public boolean isProhibitedWord(String url){
-        return prohibitedRepository.existsByWord(url);
+    public String isProhibitedWord(String url){
+        return
+                Arrays.stream(url.split("/"))
+                        .filter(word->prohibitedRepository.existsByWord(word))
+                        .findFirst()
+                        .orElse(null);
     }
 }
--- encoder/src/main/java/com/example/demo/url/UrlController.java
+++ encoder/src/main/java/com/example/demo/url/UrlController.java
@@ -3,10 +3,13 @@ package com.example.demo.url;
 import com.example.demo.prohibited.ProhibitedService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Instant;
 
 
 @RestController
@@ -15,10 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UrlController {
     private final UrlService urlService;
     private final ProhibitedService prohibitedService;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
     @PostMapping("/encode")
     public ResponseEntity<String> add(@RequestBody Url url) {
-        if (prohibitedService.isProhibitedWord(url.originName)) {
+        String prohibitedWord = prohibitedService.isProhibitedWord(url.originName);
+        if (prohibitedWord != null) {
+            kafkaTemplate.send("my-topic",
+                    String.format("date: %s, ulr: %s, word: %s",
+                            Instant.now().toString(),
+                            url.originName,
+                            prohibitedWord));
             return ResponseEntity.badRequest().body("is prohibitied");
         }
         return ResponseEntity.ok(urlService.add(url).getShortName());
